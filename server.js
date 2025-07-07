2ecfd8c5cf9092c3062c963956f7e24fde93b2db
hp <hp@LAPTOP-9EEFE7UO>
2025-07-07 13:57:27 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path'); // Import the path module
+
+const app = express();
+//const port = 3000;
+
+// Define the directory where your static HTML files are located
+const publicPath = path.join(__dirname, 'public');
+
+// Serve static files from the 'public' directory
+//app.use(express.static(publicPath));
+
+// Route to serve a specific HTML file
+app.get('/bhavini', (req, res) => {
+  app.use(express.static(publicPath));
+  res.sendFile(path.join(__dirname, 'public','index.html'));
+});
+app.get('/', (req, res) => {
+  res.write( 'HELLO, how are you');
+  res.end();
+});
+app.listen(8000)
+
+/*app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});*/
